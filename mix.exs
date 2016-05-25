59733fd14359187ae76363e2745e5ad70f4aa0b5
thomas <thomas.v.d@gmail.com>
2016-05-25 21:22:56 +0200

    Prepare first release.

--- mix.exs
+++ mix.exs
@@ -1,14 +1,21 @@
 defmodule SpaghettiPool.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :spaghetti_pool,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package,
+     name: "Spaghetti Pool",
+     docs: [source_ref: "v#{@version}"],
+     source_url: "https://github.com/theemuts/spaghetti_pool"]
   end
 
   # Configuration for the OTP application
@@ -22,19 +29,24 @@ defmodule SpaghettiPool.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:dialyze, "~> 0.2.0", only: :dev},
      # Docs dependencies
      {:earmark, "~> 0.2", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev}]
+  end
+
+  defp description do
+    """
+    A worker pool for concurrently read and written ETS table.
+    """
+  end
+
+  defp package do
+    [name: :spaghetti_pool,
+     files: ["lib", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     maintainers: ["Thomas van Doornmalen"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/theemuts/spaghetti_pool"}]
   end
 end
